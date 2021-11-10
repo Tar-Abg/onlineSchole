fc1b22bcde8473b780175f41f6bdb732debd3893
tar-abg <taronabg@mail.ru>
2021-11-10 21:57:23 +0400

    added subject as shared

--- src/app/shared/components/subjects/subjects.component.ts
+++ src/app/shared/components/subjects/subjects.component.ts
@@ -1,20 +1,19 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {StorageService} from "../../../shared/services/storage/storage.service";
-import {InfosService} from "../../../shared/services/infos/infos.service";
-import {SettingsService} from "../../../shared/services/settings/settings.service";
+import {Component, OnInit} from '@angular/core';
 import {Observable, Subscription} from "rxjs";
-import {TutorAvailabilities, TutorSubjects} from "../../../shared/models/registration.model";
-import {BasicInformation} from "../../../tutor/models/tutor.model";
+import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Categories} from "../../models/infos.model";
+import {StorageService} from "../../services/storage/storage.service";
+import {InfosService} from "../../services/infos/infos.service";
+import {SettingsService} from "../../services/settings/settings.service";
+import {TutorSubjects} from "../../models/registration.model";
 import {tap} from "rxjs/operators";
-import {Categories} from "../../../shared/models/infos.model";
 
 @Component({
   selector: 'app-subjects',
   templateUrl: './subjects.component.html',
   styleUrls: ['./subjects.component.scss']
 })
-export class SubjectsComponent implements OnInit, OnDestroy {
+export class SubjectsComponent implements OnInit {
   private readonly subscription: Subscription = new Subscription();
   form: FormGroup;
   observables: any = [{
@@ -28,13 +27,15 @@ export class SubjectsComponent implements OnInit, OnDestroy {
     private storageService: StorageService,
     private infoService: InfosService,
     private settingService: SettingsService
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
     this.formInitialization();
     this.categories$ = this.infoService.getCategories();
     this.getTutorSubjects();
   }
+
   private formInitialization(): void {
     this.form = this.fb.group({
       formArray: this.fb.array([this.createNewForm()])
@@ -43,11 +44,11 @@ export class SubjectsComponent implements OnInit, OnDestroy {
 
   createNewForm(): FormGroup {
     return this.fb.group({
-      tutorBasicInformationId: [null],
       levelId: [null],
       subjectId: [null],
       categoryId: [null],
-      userId:[this.storageService.getUserId()],
+      userId: [this.storageService.getUserId()],
+      userType: [this.storageService.getUserType()],
       id: [null]
     });
   }
@@ -70,14 +71,20 @@ export class SubjectsComponent implements OnInit, OnDestroy {
 
   onSubmit(): void {
     if (this.form.valid) {
-        this.updateTutorSubjects();
+      this.updateTutorSubjects();
     }
   }
 
   updateTutorSubjects(): void {
-    this.subscription.add(
-      this.settingService.updateTutorSubjects(this.formArray.value).subscribe()
-    );
+    if (this.storageService.getUserType() === 1) {
+      this.subscription.add(
+        this.settingService.updateTutorSubjects(this.formArray.value).subscribe()
+      );
+    } else {
+      this.subscription.add(
+        this.settingService.updateStudentSubjects(this.formArray.value).subscribe()
+      );
+    }
   }
 
   patchTutorSubjects(data: TutorSubjects[]): void {
@@ -88,7 +95,7 @@ export class SubjectsComponent implements OnInit, OnDestroy {
         const tutorSubjects = this.createNewForm();
         tutorSubjects.patchValue(item);
         this.formArray?.controls.push(tutorSubjects);
-        this.observables[this.formArray?.controls.length-1] = {
+        this.observables[this.formArray?.controls.length - 1] = {
           subjects$: this.infoService.getSubjectsByCategoryId(item.categoryId),
           levels$: this.infoService.getLevelsBySubjectId(item.subjectId),
         }
@@ -98,11 +105,19 @@ export class SubjectsComponent implements OnInit, OnDestroy {
   }
 
   getTutorSubjects(): void {
-    this.subscription.add(
-      this.settingService.getTutorSubjects(this.storageService.getUserId()).subscribe(data => {
-        this.patchTutorSubjects(data);
-      })
-    )
+    if (this.storageService.getUserType() === 1) {
+      this.subscription.add(
+        this.settingService.getTutorSubjects(this.storageService.getUserId()).subscribe(data => {
+          this.patchTutorSubjects(data);
+        })
+      );
+    } else {
+      this.subscription.add(
+        this.settingService.getStudentSubjects(this.storageService.getUserId()).subscribe(data => {
+          this.patchTutorSubjects(data);
+        })
+      );
+    }
   }
 
   categoryChange(event: any, index: number) {
@@ -129,6 +144,7 @@ export class SubjectsComponent implements OnInit, OnDestroy {
       })
     );
   }
+
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
