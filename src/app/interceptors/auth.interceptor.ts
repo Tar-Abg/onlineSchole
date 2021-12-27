4f8da7555b02bf47286c22a9c6c007405e795286
tar-abg <taronabg@mail.ru>
2021-12-27 22:52:16 +0400

    added refresh token functionality

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,46 +1,88 @@
-import { Injectable } from '@angular/core';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
 
-import {
-  HttpEvent,
-  HttpInterceptor,
-  HttpHandler,
-  HttpRequest,
-  HttpResponse
-} from '@angular/common/http'
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import {BehaviorSubject, Observable, throwError} from 'rxjs';
+import {catchError, filter, switchMap, take} from 'rxjs/operators';
 import {StorageService} from "../shared/services/storage/storage.service";
+import {AuthService} from "../shared/services/auth/auth.service";
 
 
 @Injectable()
-
 export class AuthInterceptor implements HttpInterceptor {
+  private refreshingInProgress: boolean;
+  private accessTokenSubject: BehaviorSubject<string | null> = new BehaviorSubject<string | null>(null);
 
-  constructor(
-    private storageService: StorageService
-  ) { }
+  constructor(private storageService: StorageService,
+              private authService: AuthService,
+              private router: Router) {}
 
-  // intercept request and add token
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = JSON.parse(this.storageService.getItem('auth_token'));
-    const bearer = `Bearer ${token}`; // this.anyService.getToken();
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const accessToken = JSON.parse(this.storageService.getItem('auth_token'));
 
-    if (token && !request.url.includes('/RefreshToken')) {
-      request = request.clone({
-        setHeaders: {
-          'Authorization': bearer,
+    return next.handle(this.addAuthorizationHeader(req, accessToken)).pipe(
+      catchError(err => {
+        // in case of 401 http error
+        if (err instanceof HttpErrorResponse && err.status === 401) {
+          // get refresh tokens
+          const refreshToken = JSON.stringify(this.storageService.getItem('refresh_token'));
+          // if there are tokens then send refresh token request
+          if (refreshToken && accessToken) {
+            return this.refreshToken(req, next);
+          }
+
+          // otherwise logout and redirect to login page
+          return this.logoutAndRedirect(err);
         }
-      });
+
+        // in case of 403 http error (refresh token failed)
+        if (err instanceof HttpErrorResponse && err.status === 403) {
+          // logout and redirect to login page
+          return this.logoutAndRedirect(err);
+        }
+        // if error has status neither 401 nor 403 then just return this error
+        return throwError(err);
+      })
+    );
+  }
+
+  private addAuthorizationHeader(request: HttpRequest<any>, token: string | null): HttpRequest<any> {
+    if (token) {
+      return request.clone({setHeaders: {Authorization: `Bearer ${token}`}});
     }
 
-    return next.handle(request)
-      .pipe(
-        tap(event => {
-          if (event instanceof HttpResponse) {
-          }
-        }, error => {
-          // Hadle Errors
+    return request;
+  }
+
+  private logoutAndRedirect(err: any): Observable<HttpEvent<any>> {
+    this.authService.signOut();
+    this.router.navigateByUrl('/');
+
+    return throwError(err);
+  }
+
+  private refreshToken(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (!this.refreshingInProgress) {
+      this.refreshingInProgress = true;
+      this.accessTokenSubject.next(null);
+      const refreshToken = JSON.parse(this.storageService.getItem('refresh_token'));
+      return this.authService.refreshTokens(refreshToken).pipe(
+        switchMap((res) => {
+          this.refreshingInProgress = false;
+          this.accessTokenSubject.next(res.token);
+          // repeat failed request with new token
+          return next.handle(this.addAuthorizationHeader(request, res.token));
         })
       );
-  };
+    } else {
+      // wait while getting new token
+      return this.accessTokenSubject.pipe(
+        filter(token => token !== null),
+        take(1),
+        switchMap(token => {
+          // repeat failed request with new token
+          return next.handle(this.addAuthorizationHeader(request, token));
+        }));
+    }
+  }
 }
