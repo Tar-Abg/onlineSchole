a5953d8cf89e2f905ed4b57b24439fd44450db22
tar-abg <taronabg@mail.ru>
2022-01-18 21:45:01 +0400

    tutor confirm register

--- src/app/tutor/components/confirm-register/confirm-register.component.ts
+++ src/app/tutor/components/confirm-register/confirm-register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-confirm-register',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./confirm-register.component.scss']
 })
 export class ConfirmRegisterComponent implements OnInit {
+  @Output() submit: EventEmitter<void> = new EventEmitter<void>();
 
   constructor() { }
 
