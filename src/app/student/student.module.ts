d29ce89e4b645adb890943caeb4814dd3eff32d7
Elida Maruqyan <elida.marukyan@gmail.com>
2022-05-11 23:11:50 +0400

    added date of birth field

--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -14,6 +14,7 @@ import {ReactiveFormsModule} from "@angular/forms";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatDatepickerModule} from "@angular/material/datepicker";
 import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
 
 
 @NgModule({
@@ -34,7 +35,8 @@ import {MatIconModule} from "@angular/material/icon";
     ReactiveFormsModule,
     MatFormFieldModule,
     MatDatepickerModule,
-    MatIconModule
+    MatIconModule,
+    MatInputModule
   ],
 })
 export class StudentModule {
